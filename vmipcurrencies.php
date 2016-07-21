716cc35d10affc60d96156e8483c45e8b5af3002
Ahsan Khan <bestkhan92@gmail.com>
2016-07-21 18:45:05 +0500

    Fixing Issue related to currency code

--- vmipcurrencies.php
+++ vmipcurrencies.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.VmIpCurrencies 
+ * @subpackage  System.VmIpCurrendies 
  * @copyright   Copyright (C) Creative Tech Solutions. All rights reserved.
  * @license     GNU General Public License version 2
  * @author      Ahsan Khan
@@ -40,50 +40,46 @@ class plgSystemVmIpCurrencies extends JPlugin
      * 
      * @author Ahsan Khan
      */
-    public function onAfterInitialise()
-    {    
+    public function onAfterInitialise(){    
         // Check that we are in the site application.
         if (JFactory::getApplication()->isAdmin())
         {
             return true;
         }
         $app = JFactory::getApplication();
-            $user_ip = $_SERVER['REMOTE_ADDR']; // to get current ip address
-            $details = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$user_ip));
-            $country_code =$details['geoplugin_countryCode'];
-/*
-this is what is return from above url it can be used for other purpose but i am jus using country_code
-//$user_ip='194.140.132.178'; //mexican city ip address for test
-array (
-  'geoplugin_request' => '101.50.126.116',
-  'geoplugin_status' => 200,
-  'geoplugin_credit' => 'Some of the returned data includes GeoLite data created by MaxMind, available from http://www.maxmind.com.',
-  'geoplugin_city' => 'Islamabad',
-  'geoplugin_region' => 'Islāmābād',
-  'geoplugin_areaCode' => '0',
-  'geoplugin_dmaCode' => '0',
-  'geoplugin_countryCode' => 'PK',
-  'geoplugin_countryName' => 'Pakistan',
-  'geoplugin_continentCode' => 'AS',
-  'geoplugin_latitude' => '33.6957',
-  'geoplugin_longitude' => '73.0113',
-  'geoplugin_regionCode' => '08',
-  'geoplugin_regionName' => 'Islāmābād',
-  'geoplugin_currencyCode' => 'PKR',
-  'geoplugin_currencySymbol' => '₨',
-  'geoplugin_currencySymbol_UTF8' => '₨',
-  'geoplugin_currencyConverter' => '104.703',
-)
-*/
-          
+        $user_ip = $_SERVER['REMOTE_ADDR']; // to get current ip address && this will not work in localhost write static ip for that here
 
-           $ipBaseCurrency=$country_code;
-            if (!empty($ipBaseCurrency))
-            {
-                $ipBaseCurrency = $this->getCurrencyId($ipBaseCurrency);
-                $app->setUserState( "virtuemart_currency_id", $ipBaseCurrency);
-            }
-        }
+        $details = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$user_ip));
+        $currencyCode =$details['geoplugin_currencyCode'];
+	/*
+	this is what is return from above url it can be used for other purpose but i am jus using currencyCode
+	//$user_ip='203.101.184.7'; //pakistan's city ip address for test
+	array (
+	  'geoplugin_request' => '101.50.126.116',
+	  'geoplugin_status' => 200,
+	  'geoplugin_credit' => 'Some of the returned data includes GeoLite data created by MaxMind, available from http://www.maxmind.com.',
+	  'geoplugin_city' => 'Islamabad',
+	  'geoplugin_region' => 'Islāmābād',
+	  'geoplugin_areaCode' => '0',
+	  'geoplugin_dmaCode' => '0',
+	  'geoplugin_countryCode' => 'PK',
+	  'geoplugin_countryName' => 'Pakistan',
+	  'geoplugin_continentCode' => 'AS',
+	  'geoplugin_latitude' => '33.6957',
+	  'geoplugin_longitude' => '73.0113',
+	  'geoplugin_regionCode' => '08',
+	  'geoplugin_regionName' => 'Islāmābād',
+	  'geoplugin_currencyCode' => 'PKR',
+	  'geoplugin_currencySymbol' => '₨',
+	  'geoplugin_currencySymbol_UTF8' => '₨',
+	  'geoplugin_currencyConverter' => '104.703',
+	)
+	*/
+          
+          if (!empty($currencyCode)){
+              $ipBaseCurrency = $this->getCurrencyId($currencyCode);
+              $app->setUserState( "virtuemart_currency_id", $ipBaseCurrency);
+          }
     }
     
     /*
